ef827e53c38faae19f5d6f44ef80898948e3e8c4
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-03 17:07:54 +0600

    header page

--- Prescripto/frontend/src/assets/assets.js
+++ Prescripto/frontend/src/assets/assets.js
@@ -11,6 +11,45 @@ import tick_icon from './tick_icon.svg'
 import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
+import group_profiles from './assets_frontend/group_profiles.png'
+import arrow_icon from './assets_frontend/arrow_icon.svg'
+import header_img from './assets_frontend/header_img.png'
+import appointment_img from './assets_frontend/appointment_img.png'
+import profile_pic from './assets_frontend/profile_pic.png'
+import contact_image from './assets_frontend/contact_image.png'
+import about_image from './assets_frontend/about_image.png'
+import logo from './assets_frontend/logo.svg'
+import dropdown_icon from './assets_frontend/dropdown_icon.svg'
+import menu_icon from './assets_frontend/menu_icon.svg'
+import cross_icon from './assets_frontend/cross_icon.png'
+import chats_icon from './assets_frontend/chats_icon.svg'
+import verified_icon from './assets_frontend/verified_icon.svg'
+import info_icon from './assets_frontend/info_icon.svg'
+import upload_icon from './assets_frontend/upload_icon.png'
+import stripe_logo from './assets_frontend/stripe_logo.png'
+import razorpay_logo from './assets_frontend/razorpay_logo.png'
+import doc1 from './assets_frontend/doc1.png'
+import doc2 from './assets_frontend/doc2.png'
+import doc3 from './assets_frontend/doc3.png'
+import doc4 from './assets_frontend/doc4.png'
+import doc5 from './assets_frontend/doc5.png'
+import doc6 from './assets_frontend/doc6.png'
+import doc7 from './assets_frontend/doc7.png'
+import doc8 from './assets_frontend/doc8.png'
+import doc9 from './assets_frontend/doc9.png'
+import doc10 from './assets_frontend/doc10.png'
+import doc11 from './assets_frontend/doc11.png'
+import doc12 from './assets_frontend/doc12.png'
+import doc13 from './assets_frontend/doc13.png'
+import doc14 from './assets_frontend/doc14.png'
+import doc15 from './assets_frontend/doc15.png'
+import Dermatologist from './assets_frontend/Dermatologist.svg'
+import Gastroenterologist from './assets_frontend/Gastroenterologist.svg'
+import General_physician from './assets_frontend/General_physician.svg'
+import Gynecologist from './assets_frontend/Gynecologist.svg'
+import Neurologist from './assets_frontend/Neurologist.svg'
+import Pediatricians from './assets_frontend/Pediatricians.svg'
+
 
 export const assets = {
     add_icon,
@@ -25,5 +64,43 @@ export const assets = {
     list_icon,
     tick_icon,
     appointments_icon,
-    earning_icon
+    earning_icon,
+    group_profiles,
+    arrow_icon,
+    header_img,
+    appointment_img,
+    profile_pic,
+    contact_image,
+    about_image,
+    logo,
+    dropdown_icon,
+    menu_icon,
+    cross_icon,
+    chats_icon,
+    verified_icon,
+    info_icon,
+    upload_icon,
+    stripe_logo,
+    razorpay_logo,
+    doc1,
+    doc2,
+    doc3,
+    doc4,
+    doc5,
+    doc6,
+    doc7,
+    doc8,
+    doc9,
+    doc10,
+    doc11,
+    doc12,
+    doc13,
+    doc14,
+    doc15,
+    Dermatologist,
+    Gastroenterologist,
+    General_physician,
+    Gynecologist,
+    Neurologist,
+    Pediatricians
 }
--- Prescripto/frontend/src/components/Header.jsx
+++ Prescripto/frontend/src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 py-3 md:px-10 lg:px-20'>
+        {/*------------Left Side-------------*/}
+        <div className="md:w-1/2 flex flex-col items-center justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
+            <p>Book Appointment <br/> With Trusted Doctors</p>
+            <div className="">
+                <img src={assets.group_profiles} alt="" />
+                <p className="">
+                    Simply browse through our extensive list of doctors, <br/> schedule your appointment hassle-free.
+                </p>
+            </div>
+            <a href=''>
+                Book Appointment <img src={assets.arrow_icon} alt="" className="" />
+            </a>
+        </div>
+        {/*-----------Right Side-------------*/}
+        <div className="md:w-1/2">
+            <img src={assets.header_img} alt="" />
+        </div>
+    </div>
   )
 }
 
