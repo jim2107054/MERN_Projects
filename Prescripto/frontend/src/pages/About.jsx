1a096935529cf185ed5a5b474aef495c3ddfb02b
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-06 08:08:56 +0600

    .

--- Prescripto/frontend/src/pages/About.jsx
+++ Prescripto/frontend/src/pages/About.jsx
@@ -27,17 +27,17 @@ const About = () => {
           <div className='flex flex-col gap-5 text-gray-700 font-light m-10'>
             <p className='text-xl text-gray-800'>Why <span className='text-gray-950 font-medium'>Choose Us</span></p>
             <div className='flex flex-col lg:flex-row gap-5 lg:gap-0'>
-            <div className='border border-gray-400 px-10 py-8'>
-              <p className='text-[18px] text-gray-800 font-medium mb-5'>Efficiency</p>
-              <p className='text-gray-700'>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+            <div className='border border-gray-400 hover:bg-primary group px-10 py-8'>
+              <p className='text-[18px] text-gray-800 group-hover:text-white font-medium mb-5'>Efficiency</p>
+              <p className='text-gray-700 group-hover:text-white'>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
             </div>
-            <div className='border border-gray-400 px-10 py-8'>
-              <p className='text-[18px] text-gray-800 font-medium mb-5'>Convenience</p>
-              <p className='text-gray-700'>Access to a network of trusted healthcare professionals in your area.</p>
+            <div className='border border-gray-400 hover:bg-primary group px-10 py-8'>
+              <p className='text-[18px] text-gray-800 group-hover:text-white font-medium mb-5'>Convenience</p>
+              <p className='text-gray-700 group-hover:text-white'>Access to a network of trusted healthcare professionals in your area.</p>
             </div>
-            <div className='border border-gray-400 px-10 py-8'>
-              <p className='text-[18px] text-gray-800 font-medium mb-5'>Personalization</p>
-              <p className='text-gray-700'>Tailored recommendations and reminders to help you stay on top of your health.</p>
+            <div className='border hover:bg-primary group border-gray-400 px-10 py-8'>
+              <p className='text-[18px] text-gray-800 group-hover:text-white font-medium mb-5'>Personalization</p>
+              <p className='text-gray-700 group-hover:text-white'>Tailored recommendations and reminders to help you stay on top of your health.</p>
             </div>
             </div>
           </div>
