0a33985c38775bd2f662be1cbe5673bd6fa85897
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-06 12:19:30 +0600

    .

--- Prescripto/frontend/src/pages/MyProfile.jsx
+++ Prescripto/frontend/src/pages/MyProfile.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from './../assets/assets';
 
 const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name:'jim',
+    image:assets.profile_pic,
+    email:'jim@gmail.com',
+    phone:'01700000000',
+    address:'Dhaka, Bangladesh',
+    gender:'Male',
+    age:25
+  })
+
   return (
     <div>
-       Hello I am Jim from KUET CSE. 
+       <img src={userData.image} alt="" /> 
     </div>
   )
 }
