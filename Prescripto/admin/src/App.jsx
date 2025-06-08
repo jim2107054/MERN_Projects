130901bdee05dab616288563eb2b483faa5f4209
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-08 15:21:28 +0600

    admin frontend

--- Prescripto/admin/src/App.jsx
+++ Prescripto/admin/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/AdminContext';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return (
+  const {aToken} = useContext(AdminContext)
+  return aToken ? (
+    <div className='bg-[#F8F9FD]'>
+      <Home/>
+      <ToastContainer/>
+      <Navbar/>
+    </div>
+  ):(
     <div>
       <Login/>
       <ToastContainer/>
