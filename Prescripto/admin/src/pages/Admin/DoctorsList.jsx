882391188b6f336bf5282613c2034b050e5b315b
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-10 15:59:53 +0600

    show all doctors

--- Prescripto/admin/src/pages/Admin/DoctorsList.jsx
+++ Prescripto/admin/src/pages/Admin/DoctorsList.jsx
@@ -1,14 +1,32 @@
-import React, { useContext, useEffect } from 'react'
-import { AdminContext } from '../../context/AdminContext'
+import React, { useContext, useEffect } from "react";
+import { AdminContext } from "../../context/AdminContext";
 
 const DoctorsList = () => {
-  const {doctors,getAllDoctors,aToken} = useContext(AdminContext)
-  useEffect(()=>{
+  const { doctors, getAllDoctors, aToken } = useContext(AdminContext);
+  useEffect(() => {
     getAllDoctors();
-  },[aToken])
+  }, [aToken]);
   return (
-    <div>DoctorsList</div>
-  )
-}
+    <div className="m-5 max-h-[90vh] overflow-y-scroll">
+      <h1 className="text-lg font-medium">All Doctors</h1>
+      <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
+        {doctors.length > 0 &&
+          doctors.map((item, index) => (
+            <div key={index} className="border border-blue-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group">
+              <img src={item.image} alt="" />
+              <div>
+                <p>{item.name}</p>
+                <p>{item.speciality}</p>
+                <div>
+                  <input type="checkbox" checked={item.available}/>
+                  <p>Available</p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export default DoctorsList
+export default DoctorsList;
