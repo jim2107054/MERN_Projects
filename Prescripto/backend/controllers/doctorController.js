0d3ec298cd8565fe702f0ef445cd7051af8af36b
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-11 09:42:12 +0600

    change availability property

---
+++ Prescripto/backend/controllers/doctorController.js
@@ -0,0 +1,15 @@
+import doctorModel from "./../models/DoctorModel.js";
+
+export const changeAvailability = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const docData = await doctorModel.findById(docId);
+    await doctorModel.findByIdAndUpdate(docId, {
+      available: !docData.available,
+    });
+    res.json({ success: true, message: "Availability changed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
--- Prescripto/backend/routes/adminRoute.js
+++ Prescripto/backend/routes/adminRoute.js
@@ -2,11 +2,12 @@ import express, { Router } from "express";
 import { addDoctor, adminLogin, allDoctors } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import { authAdmin } from "../middlewares/authAdmin.js";
+import { changeAvailability } from "../controllers/doctorController.js";
 
 const adminRouter = express(Router());
 
 adminRouter.post("/add-doctor",authAdmin,upload.single("doctorImage"), addDoctor);
 adminRouter.post("/admin-login", adminLogin);
 adminRouter.post("/all-doctors",authAdmin,allDoctors);//add authAdmin middleware to authenticate admin to access this api
-
+adminRouter.post("/change-availability",authAdmin,changeAvailability);//add authAdmin middleware to authenticate admin to access this api
 export default adminRouter;
