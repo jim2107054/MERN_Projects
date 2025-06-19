9fff7227eec850941aec899b43d4f05d84cf4345
jim2107054 <mdjahid205hasan@gmail.com>
2025-06-19 18:44:44 +0600

    add admin logout and doctor logout

--- Prescripto/admin/src/components/Navbar.jsx
+++ Prescripto/admin/src/components/Navbar.jsx
@@ -2,14 +2,18 @@ import React, { useContext } from 'react'
 import { assets } from './../assets/assets';
 import { AdminContext } from '../context/AdminContext';
 import { useNavigate } from 'react-router-dom';
+import { DoctorContext } from './../context/DoctorContext';
 
 const Navbar = () => {
     const {aToken,setAToken} = useContext(AdminContext);
+    const {dToken,setDToken} = useContext(DoctorContext);
     const navigate = useNavigate();
     const logout = ()=>{
         navigate('/')
         aToken && setAToken('');
         aToken && localStorage.removeItem('aToken');
+        dToken && setDToken('');
+        dToken && localStorage.removeItem('dToken');
     }
   return (
     <div className='flex justify-between items-center bg-white px-4 sm:px-10 py-3 border-b'>
@@ -22,7 +26,7 @@ const Navbar = () => {
             </p>
         </div>
         <button
-        onClick={()=>localStorage.removeItem('aToken') || setAToken(null)}
+        onClick={()=> logout()}
          className='bg-primary text-white text-sm px-10 py-2 rounded-full hover:scale-105 duration-300 transition-all'>Logout</button>
     </div>
   )
