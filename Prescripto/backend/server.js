1253ac09dd61378ad562e2b7c4fd0688001c2730
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-21 10:41:43 +0600

    modify something in server

--- Prescripto/backend/server.js
+++ Prescripto/backend/server.js
@@ -17,7 +17,12 @@ const PORT = process.env.PORT || 3000;
 uploadImageOnCloudinary();
 
 //middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+  }
+));
 app.use(express.json());
 
 //api endpoints
