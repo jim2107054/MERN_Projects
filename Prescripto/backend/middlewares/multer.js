ce650019c1327ae94b6f9cf0635d6cec4480e5d4
jim2107054 <mdjahid205hasan@gmail.com>
2025-10-22 11:25:16 +0600

    fix: Correct upload directory path in multer configuration

--- Prescripto/backend/middlewares/multer.js
+++ Prescripto/backend/middlewares/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads'); // Make sure this folder exists
+    cb(null, '../uploads'); // Make sure this folder exists
   },
   filename: (req, file, cb) => {
     cb(null,file.originalname); // Use timestamp to avoid conflicts
