c198b52dfa213c0dd03740eb5a0d463cf7c59633
jim2107054 <mdjahid205hasan@gmail.com>
2025-10-22 11:11:05 +0600

    fix: Ensure dotenv configuration is loaded at the start of the server

--- Prescripto/backend/server.js
+++ Prescripto/backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+dotenv.config();
 import path from 'path';
 import { fileURLToPath } from 'url';
 import connectDB from './config/mongoDB.js';
